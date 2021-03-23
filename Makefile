ef880c1f64bae71a1a637cf5ad4ab3f1487e2bf1
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2021-03-23 14:09:38 +0100

    #2

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC=gcc
 OUT=bin/Debug/iCrop
+IM_VERSION:=$(shell convert -version | grep Version | awk '{print $$3}' | awk -F . '{print $$1}')
 
 all: *.c
-	$(CC) -g -Wall -Wextra *.c -o $(OUT)  `pkg-config --cflags --libs MagickWand`
+	$(CC) -g -Wall -Wextra *.c -o $(OUT) -DIM_VERSION=$(IM_VERSION) `pkg-config --cflags --libs MagickWand`
--- main.c
+++ main.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wurfl.h>
-#include <ImageMagick-6/wand/MagickWand.h>
+
+#if IM_VERSION > 6
+    #include <MagickWand/MagickWand.h>
+#else
+    #include <ImageMagick-6/wand/MagickWand.h>
+#endif // IM_VERSION
+
 int main()
 {
     printf("Hello world!\n");
+    printf("%d\n", IM_VERSION);
     return 0;
 }
